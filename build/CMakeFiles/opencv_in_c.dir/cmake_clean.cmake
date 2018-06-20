cf1f263e2b8f805db054a8ef9195a958eca2d3d9
magnus@liisberg.eu <magnus@liisberg.eu>
2018-06-20 12:41:44 +0200

    CMake setup

---
+++ build/CMakeFiles/opencv_in_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_in_c.dir/src/main.c.o"
+  "opencv_in_c.pdb"
+  "opencv_in_c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/opencv_in_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
